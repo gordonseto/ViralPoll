f43799f6447d005bebdd5960829550d6b1ce4998
gordonseto <gordon.seto@gmail.com>
2016-10-11 11:05:16 -0700

    added poll creation and voting

---
+++ client/app.js
@@ -0,0 +1,17 @@
+
+Template.body.helpers({
+
+	polls: function(){
+		return Polls.find();
+	}
+
+});
+
+UI.registerHelper('indexedArray', function(context, options){
+	if (context){
+		return context.map(function(item, index){
+			item._index = index;
+			return item;
+		});
+	}
+});
